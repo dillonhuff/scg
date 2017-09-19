5b4cd5135db518f4d8b47df72d126b423ee67aa3
Dillon Huff <dillonhuff@gmail.com>
2017-09-18 21:44:35 -0700

    Code compiles and all tests pass. Lots of tests and code commented out though

--- src/geometry/polygon_3.cpp
+++ src/geometry/polygon_3.cpp
@@ -1,57 +1,57 @@
 // #include "geometry/offset.h"
-// #include "geometry/polygon_3.h"
-// #include "geometry/rotation.h"
+#include "geometry/polygon_3.h"
+#include "geometry/rotation.h"
 // //#include "geometry/vtk_debug.h"
 
-// namespace gca {
+namespace gca {
 
-//   polygon_3
-//   build_clean_polygon_3(const std::vector<point>& vertices,
-// 			const std::vector<std::vector<point>>& hole_verts) {
-//     vector<point> outer_ring = clean_vertices(vertices);
-//     delete_antennas(outer_ring);
+  polygon_3
+  build_clean_polygon_3(const std::vector<point>& vertices,
+			const std::vector<std::vector<point>>& hole_verts) {
+    vector<point> outer_ring = clean_vertices(vertices);
+    delete_antennas(outer_ring);
 
-//     // There is an occasional test failure here in simple box
-//     if (!(outer_ring.size() >= 3)) {
-//       cout << "ERROR: Outer ring size = " << outer_ring.size() << endl;
-//       //vtk_debug_ring(outer_ring);
+    // There is an occasional test failure here in simple box
+    if (!(outer_ring.size() >= 3)) {
+      cout << "ERROR: Outer ring size = " << outer_ring.size() << endl;
+      //vtk_debug_ring(outer_ring);
 
-//       DBG_ASSERT(outer_ring.size() >= 3);
-//     }
+      DBG_ASSERT(outer_ring.size() >= 3);
+    }
 
-//     vector<vector<point>> inner_rings;
-//     for (auto h : hole_verts) {
+    vector<vector<point>> inner_rings;
+    for (auto h : hole_verts) {
 
-//       auto new_h = clean_vertices(h);
-//       delete_antennas(new_h);
+      auto new_h = clean_vertices(h);
+      delete_antennas(new_h);
 
-//       if (!(new_h.size() >= 3)) {
-// 	cout << "ERROR: Inner ring size = " << h.size() << endl;
-//       } else {
-// 	inner_rings.push_back(new_h);
-//       }
-//     }
+      if (!(new_h.size() >= 3)) {
+	cout << "ERROR: Inner ring size = " << h.size() << endl;
+      } else {
+	inner_rings.push_back(new_h);
+      }
+    }
 
-//     return polygon_3(outer_ring, inner_rings, true);
-//   }
+    return polygon_3(outer_ring, inner_rings, true);
+  }
 
-//   polygon_3
-//   build_clean_polygon_3(const std::vector<point>& vertices) {
-//     return build_clean_polygon_3(vertices, {});
-//   }
+  polygon_3
+  build_clean_polygon_3(const std::vector<point>& vertices) {
+    return build_clean_polygon_3(vertices, {});
+  }
 
 
 //   // NOTE: These should be references
-//   polygon_3::polygon_3(const std::vector<point>& vertices,
-// 		       const std::vector<std::vector<point>>& hole_verts,
-// 		       const bool dummy_param) :
-//     outer_ring(vertices),
-//     inner_rings{} {
+  polygon_3::polygon_3(const std::vector<point>& vertices,
+		       const std::vector<std::vector<point>>& hole_verts,
+		       const bool dummy_param) :
+    outer_ring(vertices),
+    inner_rings{} {
 
-//     for (auto h : hole_verts) {
-//       inner_rings.push_back(h);
-//     }
-//   }
+    for (auto h : hole_verts) {
+      inner_rings.push_back(h);
+    }
+  }
   
 //   void check_simplicity(const polygon_3& p) {
 //     // check_simplicity(static_cast<const std::vector<point>&>(p.vertices()));
@@ -92,25 +92,25 @@
 //     return l;
 //   }
 
-//   boost_poly_2
-//   to_boost_poly_2(const labeled_polygon_3& p) {
-//     boost_poly_2 pr;
-//     for (auto p : p.vertices()) {
-//       boost::geometry::append(pr, boost::geometry::model::d2::point_xy<double>(p.x, p.y));
-//     }
+  boost_poly_2
+  to_boost_poly_2(const labeled_polygon_3& p) {
+    boost_poly_2 pr;
+    for (auto p : p.vertices()) {
+      boost::geometry::append(pr, boost::geometry::model::d2::point_xy<double>(p.x, p.y));
+    }
 
-//     boost::geometry::interior_rings(pr).resize(p.holes().size());
-//     for (int i = 0; i < p.holes().size(); i++) {
-//       auto& h = p.holes()[i];
-//       for (auto p : h) {
-// 	boost::geometry::append(pr, boost::geometry::model::d2::point_xy<double>(p.x, p.y), i);
-//       }
-//     }
+    boost::geometry::interior_rings(pr).resize(p.holes().size());
+    for (int i = 0; i < p.holes().size(); i++) {
+      auto& h = p.holes()[i];
+      for (auto p : h) {
+	boost::geometry::append(pr, boost::geometry::model::d2::point_xy<double>(p.x, p.y), i);
+      }
+    }
 
-//     boost::geometry::correct(pr);
+    boost::geometry::correct(pr);
     
-//     return pr;
-//   }
+    return pr;
+  }
 
 //   polygon_3
 //   to_polygon_3(const double z, const boost_poly_2& p) {
@@ -133,15 +133,15 @@
 
 //   }
 
-//   boost_poly_2 rotate_to_2D(const labeled_polygon_3& p) {
-//     const rotation r = rotate_from_to(p.normal(), point(0, 0, 1));
-//     return to_boost_poly_2(apply(r, p));
-//   }
+  boost_poly_2 rotate_to_2D(const labeled_polygon_3& p) {
+    const rotation r = rotate_from_to(p.normal(), point(0, 0, 1));
+    return to_boost_poly_2(apply(r, p));
+  }
 
-//   double area(const polygon_3& p) {
-//     auto td = rotate_to_2D(p);
-//     return bg::area(td);
-//   }
+  double area(const polygon_3& p) {
+    auto td = rotate_to_2D(p);
+    return bg::area(td);
+  }
 
 //   boost_multipoly_2
 //   planar_union_boost(const std::vector<polygon_3>& polys) {
@@ -356,93 +356,93 @@
 //     return lines;
 //   }
 
-//   bool
-//   add_to_existing_polys_as_hole(const polygon_3& next,
-// 				std::vector<polygon_3>& polygons) {
-//     if (polygons.size() == 0) { return false; }
+  bool
+  add_to_existing_polys_as_hole(const polygon_3& next,
+				std::vector<polygon_3>& polygons) {
+    if (polygons.size() == 0) { return false; }
 
-//     const rotation r = rotate_from_to(next.normal(), point(0, 0, 1));
-//     boost_poly_2 next_boost_p = to_boost_poly_2(apply(r, next));
+    const rotation r = rotate_from_to(next.normal(), point(0, 0, 1));
+    boost_poly_2 next_boost_p = to_boost_poly_2(apply(r, next));
 
-//     int max_ind = -1;
-//     double outer_area_max = -1.0;
-//     cout << "outer max = " << outer_area_max << endl;
-//     for (unsigned i = 0; i < polygons.size(); i++) {
-//       const auto& current_poly = polygons[i];
+    int max_ind = -1;
+    double outer_area_max = -1.0;
+    cout << "outer max = " << outer_area_max << endl;
+    for (unsigned i = 0; i < polygons.size(); i++) {
+      const auto& current_poly = polygons[i];
 
-//       // cout << "Current polygon" << endl;
-//       // vtk_debug_polygon(current_poly);
-//       // cout << "Both polygons" << endl;
-//       // vtk_debug_polygons({current_poly, next});
+      // cout << "Current polygon" << endl;
+      // vtk_debug_polygon(current_poly);
+      // cout << "Both polygons" << endl;
+      // vtk_debug_polygons({current_poly, next});
 
-//       boost_poly_2 current_boost_p = to_boost_poly_2(apply(r, current_poly));
-//       double outer_area = area(build_clean_polygon_3(current_poly.vertices()));
-//       cout << "outer area = " << outer_area << endl;
+      boost_poly_2 current_boost_p = to_boost_poly_2(apply(r, current_poly));
+      double outer_area = area(build_clean_polygon_3(current_poly.vertices()));
+      cout << "outer area = " << outer_area << endl;
 
-//       bool within_current = bg::within(next_boost_p, current_boost_p);
-//       cout << "within_current = " << within_current << endl;
+      bool within_current = bg::within(next_boost_p, current_boost_p);
+      cout << "within_current = " << within_current << endl;
 
-//       if (within_current && outer_area > outer_area_max) {
-// 	outer_area_max = outer_area;
-// 	max_ind = i;
-// 	cout << "set max_ind = " << max_ind << endl;
-//       }
+      if (within_current && outer_area > outer_area_max) {
+	outer_area_max = outer_area;
+	max_ind = i;
+	cout << "set max_ind = " << max_ind << endl;
+      }
 
-//       cout << "outer max = " << outer_area_max << endl;
-//     }
+      cout << "outer max = " << outer_area_max << endl;
+    }
 
-//     if (max_ind >= 0) {
-//       auto& current_poly = polygons[max_ind];
-//       unsigned old_num_holes = current_poly.holes().size();
-//       current_poly.add_hole(next.vertices());
+    if (max_ind >= 0) {
+      auto& current_poly = polygons[max_ind];
+      unsigned old_num_holes = current_poly.holes().size();
+      current_poly.add_hole(next.vertices());
 
-//       unsigned new_num_holes = polygons[max_ind].holes().size();
+      unsigned new_num_holes = polygons[max_ind].holes().size();
 
-//       DBG_ASSERT(new_num_holes == (old_num_holes + 1));
+      DBG_ASSERT(new_num_holes == (old_num_holes + 1));
 
-//       cout << "ADDED TO POLYGON AS HOLE" << endl;
+      cout << "ADDED TO POLYGON AS HOLE" << endl;
 
-//       return true;
-//     }
+      return true;
+    }
 
-//     return false;
-//   }
+    return false;
+  }
 
-//   std::vector<polygon_3>
-//   arrange_rings(const std::vector<std::vector<point>>& rings) {
-//     cout << "# of rings before arranging = " << rings.size() << endl;
-//     if (rings.size() == 0) { return {}; }
+  std::vector<polygon_3>
+  arrange_rings(const std::vector<std::vector<point>>& rings) {
+    cout << "# of rings before arranging = " << rings.size() << endl;
+    if (rings.size() == 0) { return {}; }
 
-//     vector<polygon_3> ring_polys;
-//     for (auto r : rings) {
-//       ring_polys.push_back(build_clean_polygon_3(r));
-//     }
+    vector<polygon_3> ring_polys;
+    for (auto r : rings) {
+      ring_polys.push_back(build_clean_polygon_3(r));
+    }
 
-//     vector<polygon_3> polygons;
-//     while (ring_polys.size() > 0) {
-//       auto next_poly =
-// 	max_element(begin(ring_polys), end(ring_polys),
-// 		    [](const polygon_3& l, const polygon_3& r) {
-// 		      return area(l) < area(r);
-// 		    });
+    vector<polygon_3> polygons;
+    while (ring_polys.size() > 0) {
+      auto next_poly =
+	max_element(begin(ring_polys), end(ring_polys),
+		    [](const polygon_3& l, const polygon_3& r) {
+		      return area(l) < area(r);
+		    });
 
-//       DBG_ASSERT(next_poly != end(ring_polys));
+      DBG_ASSERT(next_poly != end(ring_polys));
 
-//       polygon_3 next_p = *next_poly;
+      polygon_3 next_p = *next_poly;
 
-//       ring_polys.erase(next_poly);
+      ring_polys.erase(next_poly);
 
-//       bool is_hole = add_to_existing_polys_as_hole(next_p, polygons);
+      bool is_hole = add_to_existing_polys_as_hole(next_p, polygons);
 
-//       if (!is_hole) {
-// 	polygons.push_back(next_p);
-//       }
-//     }
+      if (!is_hole) {
+	polygons.push_back(next_p);
+      }
+    }
 
-//     cout << "# of polygons after arranging = " << polygons.size() << endl;
+    cout << "# of polygons after arranging = " << polygons.size() << endl;
 
-//     return polygons;
-//   }
+    return polygons;
+  }
 
 //   labeled_polygon_3 dilate(const labeled_polygon_3& p, const double tol) {
 //     cout << "P normal = " << p.normal() << endl;
@@ -664,4 +664,4 @@
 //     return from_boost_multipoly_2(cp, r, z_level);
 //   }
   
-// }
+}
