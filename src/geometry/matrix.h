609d694a5c18f2f56446f1ac18fb8cff097181fd
Dillon Huff <dillonhuff@gmail.com>
2017-11-01 17:46:36 -0700

    Starting to wrap up matrices

--- src/geometry/matrix.h
+++ src/geometry/matrix.h
@@ -10,26 +10,29 @@ namespace ublas = boost::numeric::ublas;
 
 namespace gca {
 
+  typedef ublas::matrix<double> matrix;
+  typedef ublas::vector<double> vec;
+
   point from_vector(ublas::vector<double> v);
   ublas::vector<double> to_vector(const point p);
 
-  double determinant(ublas::matrix<double>& m);
-  double determinant(const ublas::matrix<double>& m);
+  double determinant(matrix& m);
+  double determinant(const matrix& m);
 
-  ublas::matrix<double> inverse(ublas::matrix<double>& a);
-  ublas::matrix<double> inverse(const ublas::matrix<double>& a);
+  matrix inverse(matrix& a);
+  matrix inverse(const matrix& a);
 
-  ublas::matrix<double>
+  matrix
   plane_basis_rotation(const point at, const point bt, const point ct,
 		       const point apt, const point bpt, const point cpt);
 
   boost::numeric::ublas::vector<double>
-  plane_basis_displacement(const ublas::matrix<double>& r,
+  plane_basis_displacement(const matrix& r,
 			   const point u1, const point u2, const point u3,
 			   const point q1, const point q2, const point q3,
 			   const point p1, const point p2, const point p3);
   
-  point times_3(const ublas::matrix<double> m, const point p);
+  point times_3(const matrix m, const point p);
 
 }
 
